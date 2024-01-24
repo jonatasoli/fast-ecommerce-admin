55eac9fe0359c03574051b7a686459a8bec17281
Jonatas Oliveira <contact@jonatasoliveira.dev>
2024-01-24 12:59:31 +0100

    Initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import "../app.css";
+</script>
+
+<div class="card container mx-auto">
+  <h1 class="text-sky-400 hover:text-green-400 text-center">Fast E-COMMERCE ADMIN</h1>
+
+  <div>
+    <form class="rounded">
+      <p>Por favor coloque suas credenciais para logar</p>
+      <label class="block text-sm decoration-sky-500">Login</label>
+      <input class="border border-black" name="login" />
+      <label class="block text-sm decoration-sku-500">Senha</label>
+      <input class="border border-black" name="password" />
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" name=Login type="button">Login</button>
+    </form>
+  </div>
+</div>
