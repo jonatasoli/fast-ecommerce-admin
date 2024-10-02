ad09728464fd2496117398dc11e91e953578b012
jpcamposgda <jpcamposgda@gmail.com>
2024-10-02 10:52:59 -0400

    feat: implemented Patch in the Payment Tab, with handleSavePaymentGateway function, tested.

---
+++ src/lib/components/notification/notification.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { Toast } from 'flowbite-svelte';
+	import { slide } from 'svelte/transition';
+	import { CheckCircleSolid, CloseCircleSolid } from 'flowbite-svelte-icons';
+
+	export let message = 'Default message'; // Mensagem padrão
+	export let statusToast = false; // Controla a exibição do Toast
+	export let type = 'success';
+</script>
+
+<div class="flex gap-10">
+	{#if type === 'success'}
+		<Toast transition={slide} bind:statusToast color="green" position="top-right">
+			<CheckCircleSolid slot="icon" class="w-5 h-5" />
+			{message}
+		</Toast>
+	{/if}
+	{#if type === 'error'}
+		<Toast transition={slide} bind:statusToast color="red" position="top-right">
+			<CloseCircleSolid slot="icon" class="w-5 h-5" />
+			{message}
+		</Toast>
+	{/if}
+</div>
