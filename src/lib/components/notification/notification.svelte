cfbefacfceeddb524c3b7f028b7636a445bd6faf
jpcamposgda <jpcamposgda@gmail.com>
2024-10-04 06:22:02 -0400

    fix: the conflict was resolved

--- src/lib/components/notification/notification.svelte
+++ src/lib/components/notification/notification.svelte
@@ -3,22 +3,22 @@
 	import { slide } from 'svelte/transition';
 	import { CheckCircleSolid, CloseCircleSolid } from 'flowbite-svelte-icons';
 
-        const DEFAULT_MESSAGE = 'Default Message'
-        
-	export let message = DEFAULT_MESSAGE
+	const DEFAULT_MESSAGE = 'Default Message';
+
+	export let message = DEFAULT_MESSAGE;
 	export let statusExibitionToast = false;
 	export let type = 'success';
 </script>
 
 <div class="flex gap-10">
 	{#if type === 'success'}
-		<Toast transition={slide} bind:statusToast color="green" position="top-right">
+		<Toast transition={slide} bind:statusExibitionToast color="green" position="top-right">
 			<CheckCircleSolid slot="icon" class="w-5 h-5" />
 			{message}
 		</Toast>
 	{/if}
 	{#if type === 'error'}
-		<Toast transition={slide} bind:statusToast color="red" position="top-right">
+		<Toast transition={slide} bind:statusExibitionToast color="red" position="top-right">
 			<CloseCircleSolid slot="icon" class="w-5 h-5" />
 			{message}
 		</Toast>
