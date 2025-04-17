1158a17f0c906e94e00578510722a71388d1286d
jpcamposgda <jpcamposgda@gmail.com>
2025-04-17 17:56:25 -0400

    chore: update packages

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,15 +1,17 @@
+// src/hooks.server.ts
 import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const url = new URL(event.request.url);
 	const accessToken = event.cookies.get('access_token');
 
-	const protectedPaths = url.pathname.includes('/admin');
-	if (!accessToken && protectedPaths) {
+	const isProtectedRoute = url.pathname.startsWith('/admin');
+
+	if (!accessToken && isProtectedRoute) {
+		console.log('No token, redirecting from', url.pathname); // Debug
 		throw redirect(302, '/');
 	}
 
 	const response = await resolve(event);
-
 	return response;
 };
--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,4 +1,6 @@
-export const load = async ({ cookies }) => {
+import type { PageLoad } from './invoices/$types';
+
+export const load: PageLoad = async ({ cookies }) => {
 	const token = cookies.get('access_token');
 	const role = cookies.get('role');
 
